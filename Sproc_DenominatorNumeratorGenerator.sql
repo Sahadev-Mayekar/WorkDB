23ad91d0b008e01ad13423b94845390fe92fb70c
Sahadev-Mayekar <sahadev.m-ext@influencehealth.com>
2016-04-05 19:19:01 +0530

    first commit

---
+++ Sproc_DenominatorNumeratorGenerator.sql
@@ -0,0 +1,40 @@
+ALTER PROCEDURE DenominatoNumeratorGererator
+	@MeasureID INT,
+	@DenomType CHAR(1),
+	@AvailableParam VARCHAR(MAX) OUTPUT
+AS
+BEGIN
+-- =============================================
+-- Author:		Sahadev Mayekar
+-- Create date: 5-April-2016
+-- Description:	This procedure helps in generating parameters that are required to run Stored Procedures to execute measures
+-- =============================================
+	SET NOCOUNT ON;
+	BEGIN
+					DECLARE @DeclareSet VARCHAR(MAX),
+							@Param VARCHAR(100),
+							@Value VARCHAR(100),
+							@ParamType CHAR(1) = @DenomType,
+							@MinParamNo SMALLINT,
+							@MaxParamNo SMALLINT
+					SELECT @MinParamNo = Min(SerialNo), @MAXParamNo = Max(SerialNo)
+					FROM MeasureConditionValues
+					WHERE MeasureID = @MeasureID
+					WHILE(@MinParamNo <= @MaxParamNo)
+						BEGIN
+							
+							SELECT @DeclareSet =
+								(
+								SELECT [Parameters] + ' ' +  ISNULL(Value,'NULL') + ',' 
+								FROM MeasureConditionValues InnerMCV
+								WHERE MeasureID = @MinParamNo AND ParamterType = @DenomType
+								Order BY measureid
+								For XML Path('')) 
+							FROM MeasureConditionValues OuterMCV
+							SET @DeclareSet = 'DECLARE ' + SUBSTRING(RTRIM(@DeclareSet),1,LEN(RTRIM(@DeclareSet))-1) 
+							PRINT @DeclareSet
+							SET @MinParamNo = @MinParamNo + 1
+						END
+	END
+	RETURN
+END
---
+++ Sproc_MeasureValuesGenerator.sql
@@ -0,0 +1,51 @@
+USE [Navigate]
+GO
+
+/****** Object:  StoredProcedure [dbo].[MeasureValuesGenerator]    Script Date: 4/5/2016 6:07:23 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[MeasureValuesGenerator]
+	
+	@MeasureName VARCHAR(MAX) = 'ALL',
+	@AvailableParam VARCHAR(MAX) OUTPUT
+AS
+BEGIN
+-- =============================================
+-- Author:		Sahadev Mayekar
+-- Create date: 5-April-2016
+-- Description:	This procedure helps in generating parameters that are required to run Stored Procedures to execute measures
+-- =============================================
+	SET NOCOUNT ON;
+	DECLARE @ParameterSet VARCHAR(MAX),
+			@MinMeasureCounter SMALLINT,
+			@MaxMeasureCounter SMALLINT
+
+	-- to set starting and ending limit on loop
+	SELECT @MinMeasureCounter = MIN(MeasureID), @MaxMeasureCounter = MAX(MeasureID)
+	FROM MeasureMaster WITH(NOLOCK)
+	WHERE IsActive = 'Y'
+	
+	WHILE (@MinMeasureCounter <= @MaxMeasureCounter)
+		BEGIN
+			DECLARE @MeasureID VARCHAR(100)
+					
+			
+			SELECT @MeasureID = MeasureID 
+			FROM MeasureMaster WITH(NOLOCK)
+			WHERE MeasureID = @MinMeasureCounter
+			PRINT @MeasureID
+			
+				EXECUTE DenominatoNumeratorGererator @MinMeasureCounter, 'A', @AvailableParam = @AvailableParam OUTPUT
+
+			SET @MinMeasureCounter = @MinMeasureCounter + 1
+
+		END
+END
+
+GO
+
+
