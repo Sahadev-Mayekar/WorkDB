589b712e7052050a23afa447397d0cf78b4287a8
Sahadev-Mayekar <sahadev.m-ext@influencehealth.com>
2016-04-06 13:36:15 +0530

    TestBranch added two dots

--- Sproc_MeasureValuesGenerator.sql
+++ Sproc_MeasureValuesGenerator.sql
@@ -15,7 +15,7 @@ CREATE PROCEDURE [dbo].[MeasureValuesGenerator]
 AS
 BEGIN
 -- =============================================
--- Author:		Sahadev Mayekar.
+-- Author:		Sahadev Mayekar..
 -- Create date: 5-April-2016
 -- Description:	This procedure helps in generating parameters that are required to run Stored Procedures to execute measures
 -- =============================================
